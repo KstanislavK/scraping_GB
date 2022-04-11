fab5b305764f53f336c4f118f02b89675d46b2d0
KstanislavK <t7s9SF2FWTQfXxL>
2022-04-11 15:48:20 +0300

    Initial commit

---
+++ lesson_1/task_1.py
@@ -0,0 +1,37 @@
+"""
+Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для
+конкретного пользователя, сохранить JSON-вывод в файле *.json.
+
+"""
+
+import requests
+import json
+
+
+def get_data(user):
+
+    req = requests.get(f'https://api.github.com/users/{user}/repos')
+    data = json.loads(req.text)
+    repo_dict = {}
+    for item in data:
+        repo_dict[item['name']] = item['url']
+
+    return repo_dict
+
+
+def write_file(data, user):
+
+    with open(f'{user}_repos.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+    print('Work is done, master!')
+
+
+def main():
+    user = input('Введите логин пользователя: ')
+    data = get_data(user)
+    write_file(data, user)
+
+
+if __name__ == '__main__':
+    main()
